d343b0f886a910cecd71c7611ef29d5d8ec2404d
Muhammed Apak <muhampak25@gmail.com>
2023-08-16 23:28:42 +0300

    Hamburger menu design changed

--- src/components/Container/Projects/index.js
+++ src/components/Container/Projects/index.js
@@ -1,18 +1,37 @@
-import React from 'react'
-import { Box, Text, Grid, GridItem, Image, AspectRatio, Link, Card, CardBody, Stack, Heading, Divider, Flex } from '@chakra-ui/react';
-import project1 from "../../../assets/images/project-1.png"
-import project2 from "../../../assets/images/project-2.png"
-import project3 from "../../../assets/images/project-3.png"
+import React from "react";
+import {
+  Box,
+  Text,
+  Grid,
+  GridItem,
+  Image,
+  AspectRatio,
+  Link,
+  Card,
+  CardBody,
+  Stack,
+  Heading,
+  Divider,
+  Flex,
+} from "@chakra-ui/react";
+import project1 from "../../../assets/images/project-1.png";
+import project2 from "../../../assets/images/project-2.png";
+import project3 from "../../../assets/images/project-3.png";
+import LinesEllipsis from "react-lines-ellipsis";
 
-
-
-const ProjectCard = ({ imageSrc, title, description, technologies, hrefLink }) => {
+const ProjectCard = ({
+  imageSrc,
+  title,
+  description,
+  technologies,
+  hrefLink,
+}) => {
   return (
-   <Box>
-    <Card maxW='sm'>
-  <CardBody>
-  <Link href={hrefLink} target='_blank' >
-  <Image
+    <Box >
+      <Card maxW="sm">
+        <CardBody>
+          <Link href={hrefLink} target="_blank">
+            <Image
               src={imageSrc}
               alt={title}
               loading="lazy"
@@ -21,46 +40,52 @@ const ProjectCard = ({ imageSrc, title, description, technologies, hrefLink }) =
               height="200px" // Belirlediğiniz istediğiniz yükseklik
               objectFit="cover" // Resim boyutunu korurken gerektiğinde kırpabilir
             />
-    </Link>
-    <Stack mt='6' spacing='3'>
-      <Heading size='md'>Living room Sofa</Heading>
-      <Text>
-        This sofa is perfect for modern tropical spaces, baroque inspired
-        spaces, earthy toned spaces and for people who love a chic design with a
-        sprinkle of vintage design.
-      </Text>
-      <Text color='blue.600' fontSize='2xl'>
-        $450
-      </Text>
-    </Stack>
-  </CardBody>
-  <Divider />
-  
-</Card>
-   </Box>
+          </Link>
+          <Stack mt="6" spacing="3">
+            <Heading size="md" borderBottom={"1px"} borderColor={"gray.300"}>
+              {" "}
+              {title}{" "}
+            </Heading>
+            <Text>
+              <LinesEllipsis
+                text={description}
+                maxLine="3"
+                ellipsis="..."
+                trimRight
+                basedOn="letters"
+              />
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+      </Card>
+    </Box>
   );
 };
 
 const ProjectList = () => {
   const projects = [
     {
-      title: 'SahanePazar Color Game',
-      description: 'Description of Project 1.',
-      technologies: ['React', 'Chakra UI', 'Tailwind CSS'],
-      imageSrc: project1 ,// Replace with actual image source
+      title: "SahanePazar Color Game",
+      description:
+        "Experience the excitement as you uncover the elusive blue box among a set of mystery boxes in this engaging web-based puzzle game.",
+      technologies: ["React", "Chakra UI", "Tailwind CSS"],
+      imageSrc: project1, // Replace with actual image source
       hrefLink: "https://sahane-pazar-box-game.web.app/",
     },
     {
-      title: 'Project 2',
-      description: 'Description of Project 2.',
-      technologies: ['React', 'Chakra UI', 'Tailwind CSS'],
+      title: "Weather App",
+      description:
+        "Discover real-time weather updates and stay informed about current conditions with the Weather Insight App.",
+      technologies: ["React", "Chakra UI", "Tailwind CSS"],
       imageSrc: project2, // Replace with actual image source
-      hrefLink:"https://apakweatherapp.netlify.app/",
+      hrefLink: "https://apakweatherapp.netlify.app/",
     },
     {
-      title: 'Project 3',
-      description: 'Description of Project 3.',
-      technologies: ['React', 'Chakra UI', 'Tailwind CSS'],
+      title: "E-Commerce App",
+      description:
+        "Explore a seamless shopping journey like never before on our Online Marketplace, where you can discover a wide range of products and enjoy hassle-free online shopping.",
+      technologies: ["React", "Chakra UI", "Tailwind CSS"],
       imageSrc: project3, // Replace with actual image source
       hrefLink: "https://bizim.onrender.com/",
     },
@@ -68,13 +93,18 @@ const ProjectList = () => {
   ];
 
   return (
-    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" bg="#262626">
-    {projects.map((project, index) => (
-      <Box key={index} mx={4} my={6} flex="1 0 300px" maxWidth="300px">
-        <ProjectCard {...project} />
-      </Box>
-    ))}
-  </Flex>
+    <Flex
+      flexWrap="wrap"
+      justifyContent="center"
+      alignItems="center"
+      bg="#262626"
+    >
+      {projects.map((project, index) => (
+        <Box key={index} mx={5} my={6} flex="1 0 300px" maxWidth="300px">
+          <ProjectCard {...project} />
+        </Box>
+      ))}
+    </Flex>
   );
 };
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -23,59 +23,57 @@ function Navbar() {
   }, []);
 
   return (
-    <Box className="absolute w-full mt-16 z-10">
-    <Flex
-      className="flex bg-transparent text-3xl font-bold text-cyan-500 text-opacity-30"
-      justifyContent="space-between" // Uygun hizalamayı ayarlayın
-    >
-    
-  
-      {isMobile ? (
-        <IconButton
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          onClick={() => setIsOpen(!isOpen)}
-          size="xl"
-          variant="ghost"
-          display={{ base: "flex", md: "none" }}
-          color={"crimson"}
-          ml={5}
+    <Box className="absolute w-full  sm:mt-16 mt-0 z-10  ">
+      <Flex
+        className=" sm:justify-center justify-between  bg-transparent text-3xl font-bold text-cyan-500 text-opacity-30"
+         // Uygun hizalamayı ayarlayın
+      >
+        {isMobile ? (
+          <IconButton
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            onClick={() => setIsOpen(!isOpen)}
+            size="xl"
+            variant="ghost"
+            display={{ base: "flex", md: "none" }}
+            color={"crimson"}
+            ml={5}
+            position={"absolute"}
+            mt={5}
+            
+          />
+        ) : (
+          <Flex align="center" as="ul" listStyleType="none">
+            <NavItem to="/" label="Home Page" />
+            <NavItem to="projects" label="Projects" />
+            <NavItem to="aboutme" label="About Me" />
+            <NavItem to="contact" label="Contact" />
+          </Flex>
+        )}
+      </Flex>
 
+      {isMobile && isOpen && (
+        <Box
+          p="4"
+          pt={20}
+          pl={10}
+          display={{ base: "block", md: "none" }}
+          bg="linear-gradient(to bottom, #2193b0, #6dd5ed)"
+          color="#94716b"
+          listStyleType={"none"}
+          fontSize={"6xl"}
+          height={"100vh"}
+          justifyContent={"center"}
+          alignItems={"center"}
           
-        />
-      ) : (
-        <Flex align="center" as="ul" listStyleType="none" >
+        >
+          {/* Mobil menü içeriği */}
           <NavItem to="/" label="Home Page" />
           <NavItem to="projects" label="Projects" />
           <NavItem to="aboutme" label="About Me" />
           <NavItem to="contact" label="Contact" />
-        </Flex>
+        </Box>
       )}
-    </Flex>
-  
-    {isMobile && isOpen && (
-      <Box
-        p="4"
-        mt="4"
-        
-        display={{ base: "block", md: "none" }}
-        bg="linear-gradient(to bottom, #2193b0, #6dd5ed)"
-        color="#94716b"
-        listStyleType={"none"}
-        fontSize={"2xl"}
-        height={"100vh"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        
-      >
-        {/* Mobil menü içeriği */}
-        <NavItem  to="/" label="Home Page" />
-        <NavItem  to="projects" label="Projects" />
-        <NavItem to="aboutme" label="About Me" />
-        <NavItem to="contact" label="Contact" />
-      </Box>
-    )}
-  </Box>
-  
+    </Box>
   );
 }
 
