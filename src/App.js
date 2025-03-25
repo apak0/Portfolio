d1f103f2a79257b9b102665cedb3d8485a388725
apak0 <muhampak25@gmail.com>
2025-03-25 12:23:21 +0300

    added new cursor animation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ import AboutMe from "./components/Container/AboutMe";
 import Projects from "./components/Container/Projects";
 import Contact from "./components/Container/Contact";
 import { Element } from "react-scroll";
+import LegendaryCursor from "legendary-cursor";
+window.addEventListener("load", () => {
 
+  LegendaryCursor.init({
+      lineSize:         0.05,
+      opacityDecrement: 0.25,
+      speedExpFactor:   0.8,
+      lineExpFactor:    0.3,
+      sparklesCount:    65,
+      maxOpacity:       0.99,  // should be a number between [0 ... 1]
+      // texture1:         "http://path_to_texture",      // texture displayed on mouse hover
+      // texture2:         "http://path_to_texture",      // texture displayed on mouse click
+      // texture3:         "http://path_to_texture",      // texture displayed on sparkles
+  });
+
+});
 function App() {
   return (
     <div>
